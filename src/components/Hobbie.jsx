f7f805db205d4518ac68d54950b61191127203db
Mario Sánchez <mr.mario.sm@gmail.com>
2017-10-20 17:54:02 +0200

    Add glamourous for css-in-js components

--- src/components/Hobbie.jsx
+++ src/components/Hobbie.jsx
@@ -1,11 +1,16 @@
 // @flow
 
 import React from 'react';
+import glamourous from 'glamorous';
+
+const HobbieArticle = glamourous.article({
+  backgroundColor: 'red',
+});
 
 const Hobbie = (props: { icon: string, text: string }) => (
-  <article>
+  <HobbieArticle>
     <span>{props.icon}</span> <span>{props.text}</span>
-  </article>
+  </HobbieArticle>
 );
 
 export default Hobbie;
