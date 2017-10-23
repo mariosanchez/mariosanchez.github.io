1bbc5c98fc5d8d50be45ade5ffd27138ac7573aa
Mario Sánchez <mr.mario.sm@gmail.com>
2017-10-23 19:41:29 +0200

    Add new font for the whole page

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,11 @@ const config = {
       },
       {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]',
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'public/fonts/',
+        },
       },
     ],
   },
